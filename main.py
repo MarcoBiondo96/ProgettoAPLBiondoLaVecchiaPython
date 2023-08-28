40501ae3d31d8579315b4196837950bad6675333
MarcoBiondo96 <102834722+MarcoBiondo96@users.noreply.github.com>
2023-08-28 20:43:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ import cs_player_data
 import config
 
 app=Flask("Apl")#Creazione di un'istanza dell'applicazione Flask fa da server
-api_riot=config.RIOT_API_KEY#Prelevazione dal file config delle api key relative alle 2 case video ludiche (quella della riot va aggionata ogni 24 ore all'interno del file config.py)
+api_lol=config.LOL_API_KEY#Prelevazione dal file config delle api key relative alle 2 case video ludiche (quella della riot va aggionata ogni 24 ore all'interno del file config.py)
 api_steam=config.STEAM_API_KEY
-
+api_tft=config.TFT_API_KEY
 @app.route('/graph', methods=['POST'])#Route che permete di ricevere una richiesta di un tipologia di grafico e lo restituisce
 def graph():   
     data =json.loads(request.data)#lettura dati dal post
@@ -27,11 +27,11 @@ def graph():
 
 @app.route('/tft/<nome_ev>')#Route che permete di ricevere un richiesta tramite il metodo get , restituisce i dati relativi ad un utente specificato dal parametro del get
 def richiesta_tft(nome_ev):
-    return tft_player_data.process_player_data(api_riot, nome_ev)
+    return tft_player_data.process_player_data(api_tft, api_lol,nome_ev)
 
 @app.route('/lol/<nome_ev>')
 def richiesta_lol(nome_ev):
-    return lol_player_data.process_player_data(api_riot, nome_ev)
+    return lol_player_data.process_player_data(api_lol, nome_ev)
     
 @app.route('/processlogin/<nome_ev>')
 def dict_cs_creation(nome_ev):
